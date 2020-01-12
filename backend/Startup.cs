b502fa90ec3ef451e5cf5c7bf3c5a8bfc7226e97
Maurits de Ruiter <maurits@wr-automaten.nl>
2020-01-12 13:58:09 +0100

    Test

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -90,6 +90,7 @@ namespace backend
             // var appSettings = appSettingsSection.Get<AppSettings>();
             // var key = Encoding.ASCII.GetBytes(appSettings.Secret);
             var key = Encoding.ASCII.GetBytes(ConfigurationManager.AppSettings["SECRET"]);
+
             services.AddAuthentication(x =>
             {
                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
